3ca3ad4cf0640da10d66214cb4baf29c8a23daeb
Hugues <hugues.champetier-de-ribes@grenoble-inp.org>
2024-02-05 17:32:52 +0100

    stable

--- screens/Ajouter.js
+++ screens/Ajouter.js
@@ -1,29 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
-
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function AjouterScreen() {
- 
-    const [type, setType] = useState(Camera.Constants.Type.back);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
 
-    return (
-    <View style={{ flex: 1 }}>
-        <Camera style={{ flex: 1 }} type={type}>
-            <View style={{flex: 1, backgroundColor: 'transparent', flexDirection: 'row',}}>
-                <TouchableOpacity
-                    style={{flex: 0.1, alignSelf: 'flex-end', alignItems: 'center',}}
-                    onPress={() => {
-                        setType(
-                            type === Camera.Constants.Type.back
-                            ? Camera.Constants.Type.front
-                            : Camera.Constants.Type.back
-                        );
-                }}>
-                    <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-                </TouchableOpacity>
-            </View>
-        </Camera>
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`ISBN ${data}`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
-    );
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
--- screens/Avis.js
+++ screens/Avis.js
@@ -5,7 +5,7 @@ import { Text, View } from 'react-native';
 export default function AvisScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Laisser un avis!</Text>
+            <Text>Emprunter un livre!</Text>
         </View>
     );
 }
